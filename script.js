9025da5297142a0484d55cfa5dda7edf9f9044f4
mbaytas <m@baytas.net>
2024-09-12 10:26:39 +0200

    Step 3: Write the logic to get the human choice

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 console.log("Let the games begin!");
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+  // Use the prompt method to get the user’s input
+  let choice = prompt("Choose.");
+  choice = choice.toLowerCase();
+  // Return the choice if valid
+  if (choices.includes(choice)) return choice;
+  else return undefined;
+}
+
+console.log("Computer choice: " + getComputerChoice());
+console.log("Human choice: " + getHumanChoice());
