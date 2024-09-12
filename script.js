e29f78dea2f1dbd955ea7041c7465dc721c6842f
mbaytas <m@baytas.net>
2024-09-12 11:53:19 +0200

    improvements

--- script.js
+++ script.js
@@ -14,40 +14,38 @@ function getHumanChoice() {
   let choice = prompt("Choose.");
   choice = choice.toLowerCase();
   // Return the choice if valid
-  if (choices.includes(choice)) return choice;
-  else return "INVALID";
+  if (choices.includes(choice)) {
+    return choice;
+  } else {
+    getHumanChoice();
+  }
+}
+
+function playRound(humanChoice, computerChoice) {
+  let winner;
+  console.log("Human choice: " + humanChoice);
+  console.log("Computer choice: " + computerChoice);
+
+  // Determine and declare the winner
+  if (humanChoice == computerChoice) {
+    console.log("Tie!");
+  } else if (
+    (humanChoice == "rock" && computerChoice == "scissors") ||
+    (humanChoice == "paper" && computerChoice == "rock") ||
+    (humanChoice == "scissors" && computerChoice == "paper")
+  ) {
+    winner = "human";
+  } else {
+    winner = "computer";
+  }
+
+  return winner;
 }
 
 function playGame() {
-  // Variables
   let humanScore = 0;
   let computerScore = 0;
 
-  // Functions
-  function playRound(humanChoice, computerChoice) {
-    console.log("Human choice: " + humanChoice);
-    console.log("Computer choice: " + computerChoice);
-
-    // Determine and declare the winner
-    if (humanChoice == computerChoice) {
-      console.log("Tie!");
-    } else if (
-      (humanChoice == "rock" && computerChoice == "scissors") ||
-      (humanChoice == "paper" && computerChoice == "rock") ||
-      (humanChoice == "scissors" && computerChoice == "paper")
-    ) {
-      console.log("Human wins!");
-      humanScore++;
-    } else {
-      console.log("Computer wins!");
-      computerScore++;
-    }
-
-    // Declare the score
-    console.log("Human: " + humanScore + " | Computer: " + computerScore);
-  }
-
-  // Execute
   console.log("Let the games begin!");
 
   for (i = 1; i <= 5; i++) {
@@ -56,7 +54,17 @@ function playGame() {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
+    const winner = playRound(humanSelection, computerSelection);
+    console.log("Winner: " + winner);
+
+    if (winner == "computer") {
+      computerScore++;
+    }
+    if (winner == "human") {
+      humanScore++;
+    }
+
+    console.log("Human: " + humanScore + " | Computer: " + computerScore);
   }
 
   if (humanScore == computerScore) {
@@ -69,4 +77,12 @@ function playGame() {
 }
 
 // RUN THE GAME
+
 playGame();
+
+// TO DO
+// + move the playRound() function out of playGame() into global scope
+// + re-ask for human choice if invalid
+// - there should be 5 rounds without tie, if there are ties, increase number of rounds
+// - ask to play again
+// - UI
