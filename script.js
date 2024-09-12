96c127bdf3569a07026e92982d0ef47f099f7dcc
mbaytas <m@baytas.net>
2024-09-12 09:55:51 +0200

    Step 2: Write the logic to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Let the games begin!");
+
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice());
